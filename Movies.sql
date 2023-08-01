24593849553c627ac7d0eeb8bcb8ce5ba20196d8
Anushateggi123 <xworkzanusha@gmail.com>
2023-08-01 22:46:55 +0530

    test-push

---
+++ Movies.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE movies;
+SHOW DATABASES;
+USE movies;
+CREATE TABLE movie(id int , movie_name varchar(20) , is_hindi boolean , rate tinyint , actor varchar(20) , genre varchar(20) , durector varchar(20) , release_year int );
+DESC MOVIES;
